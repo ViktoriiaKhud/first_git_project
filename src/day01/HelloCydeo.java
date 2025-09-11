7fc1b66a660af33f71c6dcda23bee68347208b22
ViktoriiaKhud <vika.yani89@gmail.com>
2025-09-10 22:24:27 -0400

    Update HelloCydeo.java

--- src/day01/HelloCydeo.java
+++ src/day01/HelloCydeo.java
@@ -3,8 +3,9 @@ package day01;
 public class HelloCydeo {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Hello CYDEO");
-        System.out.println("Hello Programmers");
+        System.out.println("Hello Cydeo");
+        
+        //some changes are added.
+        
     }
 }
